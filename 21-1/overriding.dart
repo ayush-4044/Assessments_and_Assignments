2d132f6f30e3f13a2e4021ce4d94d86b17338ec2
Ayush Hirpara <ayushpatel1908@gmail.com>
2026-01-22 10:34:09 +0530

    commit

---
+++ 21-1/overriding.dart
@@ -0,0 +1,40 @@
+class RBI
+{
+    int roi()
+    {
+      return 0;
+    }
+}
+class SBI extends RBI
+{
+    int roi()
+    {
+      return 7;
+    }
+}
+class ICICI extends RBI
+{
+  int roi()
+  {
+    return 8;
+  }
+}
+class AXIS extends RBI
+{
+  int roi()
+  {
+    return 9;
+  }
+}
+
+void main()
+{
+  RBI r;
+  r = SBI();
+  print(r.roi());
+  r = ICICI();
+  print(r.roi());
+  r = AXIS();
+  print(r.roi());
+
+}
